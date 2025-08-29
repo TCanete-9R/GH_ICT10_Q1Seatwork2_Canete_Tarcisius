83c808fb866a81b82b22e3bc2212dc3fbdd58212
TCañete-10R <canete.tc@obmontessori.edu.ph>
2025-08-29 15:54:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from pyscript import display
+
+print('Breakfast Diners')
+
+Waffles='100Php' #integer
+Pancakes='100Php'#integer
+Hashbrowns='60Php'#integer
+Chicken_Waffles='280Php'#integer
+Orange_Juice='60Php' #integer
+Black_Coffee='60Php'#integer
+Product_listing= 'Price($)' #integer
+
+display(f'Product name {Product_listing}', target="div2")
+display(f'Waffles {Waffles}', target="div2")
+display(f'Pancakes {Pancakes}', target="div2")
+display(f'Hashbrowns {Hashbrowns}', target="div2")
+display(f'Chicken Waffles {Chicken_Waffles}', target="div2")
+display(f'Orange Juice {Orange_Juice}', target="div2")
+display(f'Black Coffee {Black_Coffee}', target="div2")
